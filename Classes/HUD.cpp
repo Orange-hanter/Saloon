505248393702aa9eff6f2820bf4e86ec299668c8
orange-hanter <danil.bel53@gmail.com>
2015-07-19 19:14:18 +0300

    Creatin of the project

---
+++ Classes/HUD.cpp
@@ -0,0 +1,10 @@
+//
+// Created by aragog on 19/07/15.
+//
+
+#include "HUD.h"
+
+
+HUD::HUD() {
+    
+}
---
+++ Classes/HUD.h
@@ -0,0 +1,17 @@
+//
+// Created by aragog on 19/07/15.
+//
+
+#ifndef MYGAME_HUD_H
+#define MYGAME_HUD_H
+
+
+class HUD {
+
+public:
+    HUD();
+
+};
+
+
+#endif //MYGAME_HUD_H
